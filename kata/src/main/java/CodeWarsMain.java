bd5405c02f69221dfe5c05c0d8f3b8d1b75cdb87
Mikhail_Khazov <mkh1991@yandex.ru>
2023-05-22 21:34:30 +0300

    Who Likes It kata

--- kata/src/main/java/CodeWarsMain.java
+++ kata/src/main/java/CodeWarsMain.java
@@ -2,10 +2,6 @@ import com.codewars.romanNumeralsEncoder.Conversion;
 
 public class CodeWarsMain {
     public static void main(String[] args) {
-        Conversion c = new Conversion();
-
-        int i = 91;
-        System.out.println(i + " = " + c.solution(i));
 
     }
 }
