50319b84c1a92eca8838ca94d93e6395eac393a2
Mikhail_Khazov <mkh1991@yandex.ru>
2023-05-24 02:17:21 +0300

    Square(n) Sum kata

--- kata/src/main/java/CodeWarsMain.java
+++ kata/src/main/java/CodeWarsMain.java
@@ -2,7 +2,6 @@ import com.codewars.squareSumm.Kata;
 
 public class CodeWarsMain {
     public static void main(String[] args) {
-        int[] m = {1, 2, 2, 6, 3};
-        System.out.println(Kata.squareSum(m));
+
     }
 }
